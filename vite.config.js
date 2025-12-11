08f6bef6519979e4c65f8c248845f4df6faf7dcf
Shubham Thapan <shubhamthapan2009@gmail.com>
2025-12-11 11:58:58 -0500

    Add GitHub Pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/shareplate/',
   plugins: [react({ include: /\.(js|jsx|ts|tsx)$/ })],
   resolve: {
     alias: {
