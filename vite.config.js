69179fe9dc8f5d9eb3dd39535e684c83423014e6
Shubham Thapan <shubhamthapan2009@gmail.com>
2025-12-11 12:11:10 -0500

    Configure custom domain and base path for Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: '/shareplate/',
+  base: '/',
   plugins: [react({ include: /\.(js|jsx|ts|tsx)$/ })],
   resolve: {
     alias: {
